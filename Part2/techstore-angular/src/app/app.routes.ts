daf18d81a6b2cbc0db9f9e6146c1bd2b21e31648
wu-shaoweidd <1785793045@qq.com>
2026-04-08 01:28:52 +0800

    Day5: Inventory service with full CRUD functionality

--- Part2/techstore-angular/src/app/app.routes.ts
+++ Part2/techstore-angular/src/app/app.routes.ts
@@ -1,14 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { InventoryComponent } from './inventory/inventory.component';
-import { SearchComponent } from './search/search.component';
-import { PrivacyComponent } from './privacy/privacy.component';
-import { HelpComponent } from './help/help.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'inventory', component: InventoryComponent },
-  { path: 'search', component: SearchComponent },
-  { path: 'privacy', component: PrivacyComponent },
-  { path: 'help', component: HelpComponent }
+  { path: '', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) },
+  { path: 'inventory', loadComponent: () => import('./inventory/inventory.component').then(m => m.InventoryComponent) },
+  { path: 'search', loadComponent: () => import('./search/search.component').then(m => m.SearchComponent) },
+  { path: 'privacy', loadComponent: () => import('./privacy/privacy.component').then(m => m.PrivacyComponent) },
+  { path: 'help', loadComponent: () => import('./help/help.component').then(m => m.HelpComponent) }
 ];
